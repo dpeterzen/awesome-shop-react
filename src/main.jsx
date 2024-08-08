b38e268d7888ff00da55c8fd56c1dbefc52820e7
Dan Peterson <dmpeterson317@gmail.com>
2024-08-08 15:00:42 -0400

    lints

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,5 @@ const router = createBrowserRouter(routes)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-
   </React.StrictMode>,
 )
